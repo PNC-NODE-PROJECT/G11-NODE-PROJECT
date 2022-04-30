749cc0bd9354b442de295b1bdaa2ed17296e7c8c
SEYHA.SOEURN <SEYHA.SOEURN@PNC.LAN>
2022-04-30 14:56:16 +0700

    #20 REGISTER ACCOUNT: user can register account

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+let isRegister = false;
+if (!isRegister) {
+    location.href = "views/register_view.html";
+}
---
+++ public/js/register_view.js
@@ -0,0 +1,25 @@
+
+
+// REGISTER ACCOUNT
+function registerAccount() {
+    if (userName && email && password) {
+        axios.post()
+    }
+}
+
+
+
+
+
+
+const userName = document.querySelector("#register-username");
+const email = document.querySelector("#register-email");
+const password = document.querySelector("#register-password");
+
+// MAIN
+const btnRegister = document.querySelector("#btn-register");
+
+
+
+btnRegister.addEventListener("click", registerAccount);
+
