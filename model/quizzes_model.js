79c0cf774fe45d7085b2b7f89cd502b2c9929b31
Sreyleak.EL <sreyleak.el@PNC.LAN>
2022-05-02 19:16:43 +0700

    #25 udate QuizSchema

--- model/quizzes_model.js
+++ model/quizzes_model.js
@@ -43,7 +43,8 @@ const quizzesSchema= new mongoose.Schema({
                 type: Number,
                 required: true
             },
-        }]
+        }],
+        required: true
     }
 });
 
